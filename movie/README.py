2f81f85ef0822b7237f48b6740254c793cbf0124
kailin <kailin@ulsee.com>
2018-03-11 20:26:57 +0800

    first try

---
+++ movie/README.py
@@ -0,0 +1,5 @@
+All the files in this folder is a closely connected entirety.
+You can execute the scrapy coding in your command line as such:
+"scrapy crawl meiju" ,by this mean you will see the log about the execution
+or "scrapy crawl meiju --nolog", if you do not want to have the execution log
+and the result will be performed in the text named "my_meiju.txt"
