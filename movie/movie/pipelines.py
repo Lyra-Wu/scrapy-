09dc58634409e6c026507e61de03aa2a75419d5c
kailin <kailin@ulsee.com>
2018-03-11 13:07:16 +0000

    modified pipelines.py

--- movie/movie/pipelines.py
+++ movie/movie/pipelines.py
@@ -18,30 +18,35 @@ class MoviePipeline(object):
         with open("my_meiju.txt",'a') as fp:
          #for item in item_jsgh:http://www.meijutt.com/content/meiju23093.html
         # for item in item_jsgh:
-            if len(item['title']) >= 1:
-                if len(item['link']) >=1:
-                    if len(item['class_']) >=1:
-                        if len(item['status']) >=1:
-                            if len(item['TV']) >=1:
-                                if len(item['update_time']) >=1:
-                                    fp.write("剧名: " + item['title'][0].encode("utf8") + '\n')
-                                    fp.write("链接: " + "http://www.meijutt.com" + item['link'][0].encode("utf8") + '\n')
-                                    fp.write("类型: " + item['class_'][0].encode("utf8") + '\n')
-                                    fp.write("更新状态: " + item['status'][0].encode("utf8") + '\n')
-                                    fp.write("电视台: " + item['TV'][0].encode("utf8") + '\n')
-                                    fp.write("更新时间: " + item['update_time'][0].encode("utf8") + '\n' + '\n')
-                                else:
-                                    pass
-                            else:
-                                pass
-                        else:
-                            pass   
-                    else:
-                        pass
-                else:
-                    pass
-            else:
-                pass
+            # if len(item['title']) >= 1:
+            #     if len(item['link']) >=1:
+            #         if len(item['class_']) >=1:
+            #             if len(item['status']) >=1:
+            #                 if len(item['TV']) >=1:
+            #                     if len(item['update_time']) >=1:
+            #                         fp.write("剧名: " + item['title'][0].encode("utf8") + '\n')
+            #                         fp.write("链接: " + "http://www.meijutt.com" + item['link'][0].encode("utf8") + '\n')
+            #                         fp.write("类型: " + item['class_'][0].encode("utf8") + '\n')
+            #                         fp.write("更新状态: " + item['status'][0].encode("utf8") + '\n')
+            #                         fp.write("电视台: " + item['TV'][0].encode("utf8") + '\n')
+            #                         fp.write("更新时间: " + item['update_time'][0].encode("utf8") + '\n' + '\n')
+
+            cnt =0
+            for key in item:
+                if len(item[key]) >0:
+                    cnt = cnt +1
+            
+            if cnt == len(item):
+                fp.write("剧名: " + item['title'][0].encode("utf8") + '\n')
+                fp.write("链接: " + "http://www.meijutt.com" + item['link'][0].encode("utf8") + '\n')
+                fp.write("类型: " + item['class_'][0].encode("utf8") + '\n')
+                fp.write("更新状态: " + item['status'][0].encode("utf8") + '\n')
+                fp.write("电视台: " + item['TV'][0].encode("utf8") + '\n')
+                fp.write("更新时间: " + item['update_time'][0].encode("utf8") + '\n' + '\n')
+                
+                    
+                
+
 
         # for item in item_jsgh:
         #     print "-------------sgh -",item
